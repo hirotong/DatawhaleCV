3fe495bb30064188a526d3d5d41308a80e054dd6
jinguang.tong <21530048@zju.edu.cn>
2020-05-22 23:24:02 +0800

    models

--- SSD_detection/model.py
+++ SSD_detection/model.py
@@ -276,3 +276,42 @@ class PredictionConvolutions(nn.Module):
         l_conv11_2 = self.loc_conv11_2(conv11_2_feats)  # (N, 16, 1, 1)
         l_conv11_2 = l_conv11_2.permute(0, 2, 3, 1).contiguous()  # (N, 1, 1, 16)
         l_conv11_2 = l_conv11_2.view(batch_size, -1, 4)  # (N, 4, 4)
+
+        # Predict classes in localization boxes
+        c_conv4_3 = self.cl_conv4_3(conv4_3_feats)  # (N, 4 * n_classes, 38, 38)
+        c_conv4_3 = c_conv4_3.permute(0, 2, 3,
+                                      1).contiguous()  # (N, 38, 38, 4 * n_classes), to match prior-box order (after .view())
+        c_conv4_3 = c_conv4_3.view(batch_size, -1,
+                                   self.n_classes)  # (N, 5776, n_classes), there are a total 5776 boxes on this feature map
+
+        c_conv7 = self.cl_conv7(conv7_feats)  # (N, 6 * n_classes, 19, 19)
+        c_conv7 = c_conv7.permute(0, 2, 3, 1).contiguous()  # (N, 19, 19, 6 * n_classes)
+        c_conv7 = c_conv7.view(batch_size, -1,
+                               self.n_classes)  # (N, 2166, n_classes), there are a total 2116 boxes on this feature map
+
+        c_conv8_2 = self.cl_conv8_2(conv8_2_feats)  # (N, 6 * n_classes, 10, 10)
+        c_conv8_2 = c_conv8_2.permute(0, 2, 3, 1).contiguous()  # (N, 10, 10, 6 * n_classes)
+        c_conv8_2 = c_conv8_2.view(batch_size, -1, self.n_classes)  # (N, 600, n_classes)
+
+        c_conv9_2 = self.cl_conv9_2(conv9_2_feats)  # (N, 6 * n_classes, 5, 5)
+        c_conv9_2 = c_conv9_2.permute(0, 2, 3, 1).contiguous()  # (N, 5, 5, 6 * n_classes)
+        c_conv9_2 = c_conv9_2.view(batch_size, -1, self.n_classes)  # (N, 150, n_classes)
+
+        c_conv10_2 = self.cl_conv10_2(conv10_2_feats)  # (N, 4 * n_classes, 3, 3)
+        c_conv10_2 = c_conv10_2.permute(0, 2, 3, 1).contiguous()  # (N, 3, 3, 4 * n_classes)
+        c_conv10_2 = c_conv10_2.view(batch_size, -1, self.n_classes)  # (N, 36, n_classes)
+
+        c_conv11_2 = self.cl_conv11_2(conv11_2_feats)  # (N, 4 * n_classes, 1, 1)
+        c_conv11_2 = c_conv11_2.permute(0, 2, 3, 1).contiguous()  # (N, 1, 1, 4 * n_classes)
+        c_conv11_2 = c_conv11_2.view(batch_size, -1, self.n_classes)  # (N, 4, n_classes)
+
+
+        # A total of 8732 boxes
+        # Concatenate in this specific order (i.e. must match the order of the prior-boxes)
+        locs = torch.cat([l_conv4_3, l_conv7, l_conv8_2, l_conv9_2, l_conv10_2, l_conv11_2], dim=1)  # (N, 8732, 4)
+        classes_scores = torch.cat([c_conv4_3, c_conv7, c_conv8_2, c_conv9_2, c_conv10_2, c_conv11_2],
+                                   dim=1)  # (N, 8732, n_classes)
+
+        return locs, classes_scores
+
+
