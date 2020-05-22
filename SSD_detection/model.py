4405ddf07757e1111f16197e550c314314bde262
jinguang.tong <21530048@zju.edu.cn>
2020-05-22 23:21:33 +0800

    models

--- SSD_detection/model.py
+++ SSD_detection/model.py
@@ -69,8 +69,8 @@ class VGGBase(nn.Module):
             state_dict[param] = pretrained_state_dict[pretrained_param_names[i]]
 
             conv_fc6_weight = pretrained_state_dict['classifier.0.weight'].view(4096, 512, 7, 7)
-            conv_fc6_bias = pretrained_state_dict['classifier.0.bias']  #(4096)
-            state_dict['conv6.weight'] = decimate(conv_fc6_weight, m=[4, None, 3, 3])   #(1024, 512, 7, 7)
+            conv_fc6_bias = pretrained_state_dict['classifier.0.bias']  # (4096)
+            state_dict['conv6.weight'] = decimate(conv_fc6_weight, m=[4, None, 3, 3])  # (1024, 512, 7, 7)
             state_dict['conv6.bias'] = decimate(conv_fc6_bias, m=[4])  # (1024)
             # fc7
             conv_fc7_weight = pretrained_state_dict['classifier.3.weight'].view(4096, 4096, 1, 1)  # (4096, 4096, 1, 1)
@@ -122,3 +122,157 @@ class VGGBase(nn.Module):
 
         # Lower-level feature maps
         return conv4_3_feats, conv7_feats
+
+
+class AuxiliaryConvolutions(nn.Module):
+    """
+    Additional convolutions to produce higher-level feature maps.
+    """
+
+    def __init__(self):
+        super(AuxiliaryConvolutions, self).__init__()
+
+        # Auxiliary/additional convolutions on the top of the VGG base
+        self.conv8_1 = nn.Conv2d(1024, 256, kernel_size=1, padding=0)  # stride = 1, by default
+        self.conv8_2 = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1)  # dim. reduction because stride > 1
+
+        self.conv9_1 = nn.Conv2d(512, 128, kernel_size=1, padding=0)
+        self.conv9_2 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)  # dim. reduction because stride > 1
+
+        self.conv10_1 = nn.Conv2d(256, 128, kernel_size=1, padding=0)
+        self.conv10_2 = nn.Conv2d(128, 256, kernel_size=3, padding=0)  # dim. reduction because padding = 0
+
+        self.conv11_1 = nn.Conv2d(256, 128, kernel_size=1, padding=0)
+        self.conv11_2 = nn.Conv2d(128, 256, kernel_size=3, padding=0)  # dim. reduction because padding = 0
+
+        # Initialize convolutions' parameters
+        self.init_conv2d()
+
+    def init_conv2d(self):
+        """
+        Initialize convolution parameters
+        :return:
+        """
+        for c in self.children():
+            if isinstance(c, nn.Conv2d):
+                nn.init.xavier_normal_(c.weight)
+                nn.init.constant_(c.bias, 0.)
+
+    def forward(self, conv7_feats):
+        """
+        Forward propagation.
+
+        :param conv7_feats: lower-level conv7 feature map, a tensor of dimensions (N, 1024, 19, 19)
+        :return: higher-level feature maps conv8_2, conv9_2, conv10_2, and conv11_2
+        """
+        out = F.relu(self.conv8_1(conv7_feats))  # (N, 256, 19, 19)
+        out = F.relu(self.conv8_2(out))  # (N, 512, 10, 10)
+        conv8_2_feats = out  # (N, 512, 10, 10)
+
+        out = F.relu(self.conv9_1(out))  # (N, 128, 10, 10)
+        out = F.relu(self.conv9_2(out))  # (N, 256, 5, 5)
+        conv9_2_feats = out  # (N, 256, 5, 5)
+
+        out = F.relu(self.conv10_1(out))  # (N, 128, 5, 5)
+        out = F.relu(self.conv10_2(out))  # (N, 256, 3, 3)
+        conv10_2_feats = out  # (N, 256, 3, 3)
+
+        out = F.relu(self.conv11_1(out))  # (N, 128, 3, 3)
+        conv11_2_feats = F.relu(self.conv11_2(out))  # (N, 256, 1, 1)
+
+        # Higher-level feature maps
+        return conv8_2_feats, conv9_2_feats, conv10_2_feats, conv11_2_feats
+
+
+class PredictionConvolutions(nn.Module):
+    """
+    Convolutions to predict class scores and bounding boxes using lower and higher-level feature maps.
+
+    The bounding boxes (locations) are predicted as encoded offsets w.r.t each of the 8732 prior (default) boxes.
+    See 'cxcy_to_gcxgcy' in utils.py for the encoding definition.
+
+    The class scores represent the scores of each object class in each of the 8732 bounding boxes located.
+    A high score for 'background' = no object.
+    """
+
+    def __init__(self, n_classes):
+        """
+        :param n_classes: number of different types of objects
+        """
+        super(PredictionConvolutions, self).__init__()
+
+        self.n_classes = n_classes
+
+        # Number of prior-boxes we are considering per position in each feature map
+        n_boxes = {
+            'conv4_3': 4,
+            'conv7': 6,
+            'conv8_2': 6,
+            'conv9_2': 6,
+            'conv10_2': 4,
+            'conv11_2': 4
+        }
+        # 4 prior-boxes implies we use 4 different aspect ratios, etc.
+
+        # Localization prediction convolutions (predict offsets w.r.t prior-boxes)
+        self.loc_conv4_3 = nn.Conv2d(512, n_boxes['conv4_3'] * 4, kernel_size=3, padding=1)
+        self.loc_conv7 = nn.Conv2d(1024, n_boxes['conv7'] * 4, kernel_size=3, padding=1)
+        self.loc_conv8_2 = nn.Conv2d(512, n_boxes['conv8_2'] * 4, kernel_size=3, padding=1)
+        self.loc_conv9_2 = nn.Conv2d(256, n_boxes['conv9_2'] * 4, kernel_size=3, padding=1)
+        self.loc_conv10_2 = nn.Conv2d(256, n_boxes['conv10_2'] * 4, kernel_size=3, padding=1)
+        self.loc_conv11_2 = nn.Conv2d(256, n_boxes['conv11_2'] * 4, kernel_size=3, padding=1)
+
+        # Class prediction convolutions (predict classes in localization boxes)
+        self.cl_conv4_3 = nn.Conv2d(512, n_boxes['conv4_3'] * n_classes, kernel_size=3, padding=1)
+        self.cl_conv7   = nn.Conv2d(1024, n_boxes['conv7'] * n_classes, kernel_size=3, padding=1)
+        self.cl_conv8_2 = nn.Conv2d(512, n_boxes['conv8_2'] * n_classes, kernel_size=3, padding=1)
+        self.cl_conv9_2 = nn.Conv2d(256, n_boxes['conv9_2'] * n_classes, kernel_size=3, padding=1)
+        self.cl_conv10_2 = nn.Conv2d(256, n_boxes['conv10_2'] * n_classes, kernel_size=3, padding=1)
+        self.cl_conv11_2 = nn.Conv2d(256, n_boxes['conv11_2'] * n_classes, kernel_size=3, padding=1)
+
+        # Initialize convolutions' parameters
+        self.init_conv2d()
+
+    def init_conv2d(self):
+        for c in self.children():
+            if isinstance(c, nn.Conv2d):
+                nn.init.xavier_normal_(c.weight)
+                nn.init.constant_(c.bias, 0.)
+
+    def forward(self, conv4_3_feats, conv7_feats, conv8_2_feats, conv9_2_feats, conv10_2_feats, conv11_2_feats):
+        """
+        Forward propagation.
+        :param conv4_3_feats: conv4_3 feature map, a tensor of dimensions (N, 512, 38, 38)
+        :param conv7_feats: conv7 feature map, a tensor of dimensions (N, 1024, 19, 19)
+        :param conv8_2_feats: conv8_2 feature map, a tensor of dimensions (N, 512, 10, 10)
+        :param conv9_2_feats: conv9_2 feature map, a tensor of dimensions (N, 256, 5, 5)
+        :param conv10_2_feats: conv10_2 feature map, a tensor of dimensions (N, 256, 3, 3)
+        :param conv11_2_feats: conv11_2 feature map, a tensor of dimensions (N, 256, 1, 1)
+        :return: 8732 locations and class scores (i.e. w.r.t each prior box) for each image
+        """
+        batch_size = conv4_3_feats.size(0)
+
+        # Predict localization boxes' bounds (as offsets w.r.t prior-boxes)
+        l_conv4_3 = self.loc_conv4_3(conv4_3_feats)     # (N, 16, 38, 38)
+        l_conv4_3 = l_conv4_3.permute(0, 2, 3, 1).contiguous()  # (N, 38, 38, 16)
+        l_conv4_3 = l_conv4_3.view(batch_size, -1, 4)   # (N, 5776, 4), there are a total 5776 boxes on this feature map
+
+        l_conv7 = self.loc_conv7(conv7_feats)   # (N, 24, 19, 19)
+        l_conv7 = l_conv7.permute(0, 2, 3, 1).contiguous()  #(N, 19, 19, 24)
+        l_conv7 = l_conv7.view(batch_size, -1, 4)   #(N, 2661, 4)
+
+        l_conv8_2 = self.loc_conv8_2(conv8_2_feats)  # (N, 24, 10, 10)
+        l_conv8_2 = l_conv8_2.permute(0, 2, 3, 1).contiguous()  # (N, 10, 10, 24)
+        l_conv8_2 = l_conv8_2.view(batch_size, -1, 4)  # (N, 600, 4)
+
+        l_conv9_2 = self.loc_conv9_2(conv9_2_feats)  # (N, 24, 5, 5)
+        l_conv9_2 = l_conv9_2.permute(0, 2, 3, 1).contiguous()  # (N, 5, 5, 24)
+        l_conv9_2 = l_conv9_2.view(batch_size, -1, 4)  # (N, 150, 4)
+
+        l_conv10_2 = self.loc_conv10_2(conv10_2_feats)  # (N, 16, 3, 3)
+        l_conv10_2 = l_conv10_2.permute(0, 2, 3, 1).contiguous()  # (N, 3, 3, 16)
+        l_conv10_2 = l_conv10_2.view(batch_size, -1, 4)  # (N, 36, 4)
+
+        l_conv11_2 = self.loc_conv11_2(conv11_2_feats)  # (N, 16, 1, 1)
+        l_conv11_2 = l_conv11_2.permute(0, 2, 3, 1).contiguous()  # (N, 1, 1, 16)
+        l_conv11_2 = l_conv11_2.view(batch_size, -1, 4)  # (N, 4, 4)
